89569cce62378fcca357160e92a6ef19e885a99a
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-25 14:55:17 +0530

    [ADD] Patient class added with variables

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -1,4 +1,60 @@
 package models;
 
 public class Patient {
+
+    public int patientId;
+    public String name;
+    public int age;
+    public String gender;
+    public String phoneNumber;
+    public int userId;
+
+    public Patient(int patientId,String name,int age,String gender,String phoneNumber,int userId)
+    {
+        this.age=age;
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+        this.patientId=patientId;
+        this.gender=gender;
+        this.userId=userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String toString()
+    {
+        return "Patient ID : " +patientId +
+                "Name : " + name +
+                "Age : " + age +
+                "Gender :" + gender +
+                "Phone :" +phoneNumber;
+    }
+
+
 }
