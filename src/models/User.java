4e9d1add0a7dfbfce3c68854a2936628d6a16aab
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-25 14:40:15 +0530

    [ADD] User model added

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,42 @@
 package models;
 
 public class User {
+
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(int userId, String username, String password, String role) {
+        this.userId = userId;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "UserId :" + userId + "UserName :" + username + "role :" + role;
+    }
 }
