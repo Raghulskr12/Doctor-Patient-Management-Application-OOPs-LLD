89638777ec5e629fed7fa6dc9d57974f347994da
Raghulskr12 <kalairaghul70@gmail.com>
2026-01-28 22:19:42 +0530

    [ADD] medical record attributes alone added

--- src/models/MedicalRecord.java
+++ src/models/MedicalRecord.java
@@ -1,4 +1,13 @@
 package models;
 
 public class MedicalRecord {
+    private int recordId;
+    private int patientId;
+    private int doctorId;
+    private String description;
+    private String diagnosis;
+    private String date;
+
+
+    public MedicalRecord(int recordId,int )
 }
