c513878abc02484332ea624041785d1081c4989e
Raghulskr12 <kalairaghul70@gmail.com>
2026-02-02 20:37:35 +0530

    [ADD] Medical record class added

--- src/models/MedicalRecord.java
+++ src/models/MedicalRecord.java
@@ -9,5 +9,47 @@ public class MedicalRecord {
     private String date;
 
 
-    public MedicalRecord(int recordId,int )
+    public MedicalRecord(int recordId, int patientId, int doctorId, String description, String diagnosis, String date)
+    {
+        this.recordId=recordId;
+        this.patientId=patientId;
+        this.doctorId=doctorId;
+        this.description=description;
+        this.diagnosis=diagnosis;
+        this.date=date;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getRecordId() {
+        return recordId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public String toString()
+    {
+        return  "Record Id :" + recordId +
+                "Patient Id : " + patientId +
+                "Doctor Id : " + doctorId +
+                "Description : " + description +
+                "Diagnosis : " + diagnosis +
+                "Date : " + date;
+    }
 }
